aaf123575df0de40900e582971a5b670c782ff68
jackson <jackson.severino.rocha@escola.pr.gov.br>
2021-12-29 10:10:04 -0300

    Refatorando testes de ListaDeMateriaisTest

--- Domain/tests/SolicitacaoDeMateriais/ListaDeMateriaisTest.java
+++ Domain/tests/SolicitacaoDeMateriais/ListaDeMateriaisTest.java
@@ -10,32 +10,48 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListaDeMateriaisTest {
-    public ListaDeMateriaisEntity listaDeMateriaisEntity;
+    public ListaDeMateriaisEntity listaDeMateriaisEntity = new ListaDeMateriaisEntity();
     public ListaDeMateriais listaDeMateriais;
+    public ArrayList<ListaDeMateriaisEntity> arrayList;
+    public String id = UUID.randomUUID().toString();
+    public int quantidade = 4;
+    public String produto = "Novo Produto";
+
+    public ListaDeMateriaisTest() {
+        this.listaDeMateriaisEntity.setId(this.id);
+        this.listaDeMateriaisEntity.setQuantidade(this.quantidade);
+        this.listaDeMateriaisEntity.setProduto(this.produto);
+        this.listaDeMateriais = new ListaDeMateriais();
+        this.listaDeMateriais.addMateriais(this.listaDeMateriaisEntity);
+    }
 
     @Test
     public void testeSeECriadoUmObjetoListaDeMateriais() {
-        this.listaDeMateriaisEntity = new ListaDeMateriaisEntity();
-        this.listaDeMateriais = new ListaDeMateriais();
-
         assertEquals(ListaDeMateriais.class, this.listaDeMateriais.getClass());
     }
 
     @Test
     public void testeSeECriadoUmaListaDeMateriais() {
-        String id = UUID.randomUUID().toString();
-        int quantidade = 4;
-        String produto = "Novo Produto";
-        this.listaDeMateriaisEntity = new ListaDeMateriaisEntity();
-        this.listaDeMateriaisEntity.setId(id);
-        this.listaDeMateriaisEntity.setQuantidade(quantidade);
-        this.listaDeMateriaisEntity.setProduto(produto);
-        this.listaDeMateriais = new ListaDeMateriais();
+        this.arrayList = this.listaDeMateriais.pegarListaDeMateriais();
 
-        this.listaDeMateriais.addMateriais(this.listaDeMateriaisEntity);
-        ArrayList<ListaDeMateriaisEntity> arrayList;
-        arrayList = this.listaDeMateriais.pegarListaDeMateriais();
-
-        assertEquals(ArrayList.class, arrayList.getClass());
+        assertEquals(ArrayList.class, this.arrayList.getClass());
     }
+
+    @Test
+    public void testeSeListaDeMateriaisContemOsItensAdcionados() {
+        this.arrayList = this.listaDeMateriais.pegarListaDeMateriais();
+
+        assertEquals(id, this.arrayList.get(0).getId());
+        assertEquals(quantidade, this.arrayList.get(0).getQuantidade());
+        assertEquals(produto, this.arrayList.get(0).getProduto());
+    }
+
+//    @Test
+//    public void testeSeListaDeMateriaisEAtualizadaPeloIndice() {
+//        this.arrayList = this.listaDeMateriais.pegarListaDeMateriais();
+//
+//        assertEquals(id, this.arrayList.get(0).getId());
+//        assertEquals(quantidade, this.arrayList.get(0).getQuantidade());
+//        assertEquals(produto, this.arrayList.get(0).getProduto());
+//    }
 }
