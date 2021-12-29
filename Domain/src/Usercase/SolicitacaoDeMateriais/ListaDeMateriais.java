46ff14824b1ff0aa19ee1d7606b8700d72fc7683
jackson <jackson.severino.rocha@escola.pr.gov.br>
2021-12-29 09:45:12 -0300

    add ListaDeMateriais

--- Domain/src/Usercase/SolicitacaoDeMateriais/ListaDeMateriais.java
+++ Domain/src/Usercase/SolicitacaoDeMateriais/ListaDeMateriais.java
@@ -2,11 +2,20 @@ package src.Usercase.SolicitacaoDeMateriais;
 
 import src.Entitie.ListaDeMateriaisEntity;
 
+import java.util.ArrayList;
+
 public class ListaDeMateriais {
-    private ListaDeMateriaisEntity listaDeMateriaisEntity;
+    private ArrayList<ListaDeMateriaisEntity> listaDeMateriais;
 
+    public ListaDeMateriais() {
+        this.listaDeMateriais = new ArrayList<>();
+    }
 
-    public ListaDeMateriaisEntity pegarListaDeMateriais() {
-        return this.listaDeMateriaisEntity;
+    public void addMateriais(ListaDeMateriaisEntity listaDeMateriaisEntity) {
+        this.listaDeMateriais.add(listaDeMateriaisEntity);
+    }
+
+    public ArrayList<ListaDeMateriaisEntity> pegarListaDeMateriais() {
+        return this.listaDeMateriais;
     }
 }
